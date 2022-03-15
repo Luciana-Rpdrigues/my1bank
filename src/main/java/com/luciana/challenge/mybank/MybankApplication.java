45ffcddb0dc7615de7cebd9610031fbc37d4986b
luciana <lucianna.adm2010@gmail.com>
2022-03-15 19:43:58 -0300

    Initial project

---
+++ src/main/java/com/luciana/challenge/mybank/MybankApplication.java
@@ -0,0 +1,13 @@
+package com.luciana.challenge.mybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luciana/challenge/mybank/MybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciana.challenge.mybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
